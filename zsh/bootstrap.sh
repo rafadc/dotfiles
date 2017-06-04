87863dee3081e32ae786e2181b4fa189f26e117b
Rafa de Castro <rafael@micubiculo.com>
2017-06-04 09:55:25 +0200

    Starting to make this work again

---
+++ zsh/bootstrap.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/zsh/zlogin ~/.zlogin
+ln -s ~/.dotfiles/zsh/zlogout ~/.zlogout
+ln -s ~/.dotfiles/zsh/zprofile ~/.zprofile
+ln -s ~/.dotfiles/zsh/zshenv ~/.zshenv
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
