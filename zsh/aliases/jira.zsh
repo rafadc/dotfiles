65e72edd076d426e3de6fa4dfa3e4b5ebcb2b3f7
Rafael de Castro <rafael@micubiculo.com>
2019-05-08 10:32:42 +0200

    Updating aliases for stuart

--- zsh/aliases/jira.zsh
+++ zsh/aliases/jira.zsh
@@ -1,3 +1,18 @@
 jr() {
-  jira $(git branch | sed -n '/* /s///p' | sed -n '/PFM/p' | awk -F. '{ print $1 }')
+    ISSUES=$(jira list --query="project = POOL order by lastViewed DESC")
+    echo $ISSUES | fzf --no-sort --reverse \
+                       --preview='jira view $(echo {} | cut -d ':' -f 1)' \
+                       --header "enter to view, ctrl-o to open in browser" \
+                       --bind 'enter:execute(ISSUE=$(echo {} | cut -d ':' -f 1); jira view $ISSUE | less)' \
+                       --bind 'ctrl-o:execute(ISSUE=$(echo {} | cut -d ':' -f 1); jira browse $ISSUE)' \
+                       --preview-window=down:60%
+}
+
+jbr() {
+    ISSUE_CODE=$(_jira_select_issue)
+    ISSUE_DESCRIPTION=$(jira view $ISSUE_CODE | grep "summary:" | cut -d ':' -f 2)
+    NON_LETTERS_OR_DIGITS="/[^A-Za-z0-9._-]/"
+    NORMALIZED_DESCRIPTION=$(echo $ISSUE_DESCRIPTION | awk '{$1=$1};1' | awk '{gsub('$NON_LETTERS_OR_DIGITS',"-");print}')
+    BRANCH=$ISSUE_CODE"-rafa-"$NORMALIZED_DESCRIPTION
+    git checkout develop && git pull && git branch $BRANCH && git checkout $BRANCH
 }
