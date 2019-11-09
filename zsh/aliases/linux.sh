15b4b85acec997d8fbf86e14640514c8815f4726
Rafa de Castro <rafael@micubiculo.com>
2019-11-09 21:10:26 +0100

    Added command to start EOS as a webcam

--- zsh/aliases/linux.sh
+++ zsh/aliases/linux.sh
@@ -1,4 +1,6 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     alias pbcopy='xclip -selection clipboard'
     alias pbpaste='xclip -selection clipboard -o'
+
+    alias eosm50_capture="gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video4"
 fi
