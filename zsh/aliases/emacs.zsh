4400f16707b4d4b80d456b6449e5274f7485018e
Rafa de Castro <rafael@micubiculo.com>
2019-12-01 14:46:49 +0100

    I need this in Linux to avoid window decorations in all cases

--- zsh/aliases/emacs.zsh
+++ zsh/aliases/emacs.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 alias edaemon='emacs --daemon'
-alias e='emacsclient --no-wait -c'
+alias e="emacsclient -e \"(set-frame-parameter nil 'undecorated t)\" --no-wait -c"
 alias et='emacsclient -nw'
 alias emacskill='emacsclient -e "(kill-emacs)"'
