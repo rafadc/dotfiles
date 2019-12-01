42b18ceb66bda3718624d5d0cddcc937efac67f1
Rafael de Castro <rafael@micubiculo.com>
2019-12-01 10:12:54 +0100

    Moving mac aliases to mac only section

--- zsh/aliases/index.zsh
+++ zsh/aliases/index.zsh
@@ -8,13 +8,9 @@ alias prp="pipenv run python"
 # General Unix aliases
 # -----------------------
 
-alias brewfullupdate="brew update && brew upgrade && brew cleanup -s --prune=1"
-
 alias serve="ruby -run -e httpd . -p 9090"
 
 alias aria2c='aria2c --file-allocation=none -c -x 10 -s 10'
-
-alias c="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
 
 alias blink='blink1-tool --blink'
 
@@ -25,6 +21,9 @@ alias cat=bat
 # --------------------
 
 if [[ `uname` == 'Darwin' ]]; then
+    alias c="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
+    alias brewfullupdate="brew update && brew upgrade && brew cask upgrade && brew cleanup -s --prune=1"
+
     alias ls='/usr/local/bin/gls --color -a'
     alias speedup_backups="sudo sysctl debug.lowpri_throttle_enabled=0"
 fi
