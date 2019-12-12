c89c09cd1b29117c280c81e298ad2368610555ef
Rafa de Castro <rafael@micubiculo.com>
2019-12-12 12:07:09 +0100

    Much more ellegand way of handling emacsclient

--- zsh/aliases/emacs.zsh
+++ zsh/aliases/emacs.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 alias edaemon='emacs --daemon'
-alias e="emacsclient -e \"(set-frame-parameter nil 'undecorated t)\" --no-wait -c"
+alias e='emacsclient --frame-parameters="((undecorated . t))" --no-wait --create-frame'
 alias et='emacsclient -nw'
 alias emacskill='emacsclient -e "(kill-emacs)"'
