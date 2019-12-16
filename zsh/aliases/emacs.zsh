46bba09c6670a0a3732c4b0043b2ed5ce808e066
Rafael de Castro <rafael@micubiculo.com>
2019-12-16 10:19:24 +0100

    ATM frames in mac don't work correctly

--- zsh/aliases/emacs.zsh
+++ zsh/aliases/emacs.zsh
@@ -1,6 +1,13 @@
 #!/usr/bin/env zsh
 
 alias edaemon='emacs --daemon'
-alias e='emacsclient --frame-parameters="((undecorated . t))" --no-wait --create-frame'
 alias et='emacsclient -nw'
 alias emacskill='emacsclient -e "(kill-emacs)"'
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  alias e='emacsclient --frame-parameters="((undecorated . t))" --no-wait --create-frame'
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  alias e='emacsclient --no-wait --create-frame'
+fi
