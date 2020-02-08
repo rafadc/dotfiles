d0c804263dd5b3c0fe83bebcdaf0201b0b385f81
Rafa de Castro <rafael@micubiculo.com>
2020-02-08 11:41:48 +0100

    Added tool versions to versioning

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -31,6 +31,8 @@ chsh -s "$(which zsh)"
 
 touch ~/.dotfiles/zsh/zsecrets
 
+ln -fs ~/.dotfiles/tool-versions ~/.tool-versions
+
 ln -fs ~/.dotfiles/zsh/zlogin ~/.zlogin
 ln -fs ~/.dotfiles/zsh/zlogout ~/.zlogout
 ln -fs ~/.dotfiles/zsh/zprofile ~/.zprofile
@@ -38,6 +40,7 @@ ln -fs ~/.dotfiles/zsh/zshenv ~/.zshenv
 ln -fs ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -fs ~/.dotfiles/zsh/fzf.zsh ~/.fzf.zsh
 ln -fs ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+
 ln -fs ~/.dotfiles/ruby/pryrc ~/.pryrc
 
 mkdir -p ~/.config
