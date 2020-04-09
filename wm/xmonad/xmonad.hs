479cd118698dc5be58c20029750bd8d7b6b492f4
Rafa de Castro <rafael@micubiculo.com>
2020-04-09 20:10:19 +0200

    Polishing vifm
    Starting at home
    Consistent color scheme

--- wm/xmonad/xmonad.hs
+++ wm/xmonad/xmonad.hs
@@ -24,7 +24,7 @@ main = do
 myKeys = [
            ("M-p", spawn "rofi -show run"),
            ("M-S-p", spawn "dmenu_run"),
-           ("M-n", spawn "alacritty -e vifm")
+           ("M-n", spawn "alacritty -e vifm ~")
          ]
 
 myTerminal :: String
