9fd813a11e16c58032c60d3cba6ccf50a3ad3858
Rafa de Castro <rafael@micubiculo.com>
2020-04-09 20:00:20 +0200

    Added vifm and using as default file manager

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -64,6 +64,7 @@ mkdir -p ~/.config
 ln -fs ~/.dotfiles/config/ranger ~/.config/
 ln -fs ~/.dotfiles/config/alacritty ~/.config/
 ln -fs ~/.dotfiles/config/devilspie ~/.devilspie
+ln -fs ~/.dotfiles/config/vifm ~/.config/vifm
 
 echo "Setting up tmux..."
 ln -fs ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
