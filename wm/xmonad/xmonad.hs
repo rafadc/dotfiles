cb6775bf2f71b5a6e41349a2fcc093ab85d5691e
Rafa de Castro <rafael@micubiculo.com>
2020-05-15 18:45:01 +0200

    Added named scratchpads

--- wm/xmonad/xmonad.hs
+++ wm/xmonad/xmonad.hs
@@ -8,12 +8,13 @@ import XMonad.Layout.Gaps
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Dzen
 import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.StackSet as W
+import XMonad.Util.NamedScratchpad
 
 main = do
-  -- spawn "~/.dotfiles/wm/xmonad/dzen.sh"
   xmonad $ defaultConfig
     {
-      workspaces  = ["1:dev","2:comm","3","4","5","6","7","8","9:stream","0","-","="]
+      XMonad.workspaces  = ["1:dev","2:comm","3","4","5","6","7","8","9","0","-","="]
     , terminal    = myTerminal
     , modMask     = myModMask
     , borderWidth = myBorderWidth
@@ -24,10 +25,32 @@ main = do
 myKeys = [
            ("M-p", spawn "rofi -show run"),
            ("M-S-p", spawn "dmenu_run"),
-           ("M-n", spawn "alacritty -e vifm ~"),
            ("<Print>", spawn "flameshot gui"),
-           ("M-e", spawn "rofimoji")
+           ("M-n", namedScratchpadAction scratchpads "vifm"),
+           ("M-S-n", spawn "alacritty -e vifm ~"),
+           ("M-e", spawn "rofimoji"),
+           ("M-t", namedScratchpadAction scratchpads "notes"),
+           ("M-S-s", namedScratchpadAction scratchpads "pavucontrol"),
+           ("M-i", namedScratchpadAction scratchpads "hexchat"),
+           ("M-s", namedScratchpadAction scratchpads "slack")
          ]
+
+scratchpads :: [NamedScratchpad]
+scratchpads = [
+                NS "vifm" "alacritty --class vifm -e vifm" (resource =? "vifm") centered,
+                NS "notes" "alacritty --class notes -e vim ~/notes.md" (resource =? "notes") centered,
+                NS "pavucontrol" "pavucontrol" (resource =? "pavucontrol") centeredSmall,
+                NS "hexchat" "hexchat" (resource =? "hexchat") centered,
+                NS "slack" "slack" (resource =? "slack") centered
+              ]
+              where
+                centered = customFloating $ W.RationalRect 0.9 0.9 0.05 0.05
+                centeredSmall = customFloating $ W.RationalRect l t w h
+                  where
+                    h = 0.6       -- height, 60%
+                    w = 0.6       -- width, 60%
+                    t = (1 - h)/2 -- centered top/bottom
+                    l = (1 - w)/2 -- centered left/right
 
 myTerminal :: String
 myTerminal    = "alacritty"
