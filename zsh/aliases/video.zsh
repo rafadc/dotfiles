bde7bffe89a3defa384203e7d533110e0c3abcb0
Rafa de Castro <rafael@micubiculo.com>
2020-10-07 20:42:54 +0200

    Trying to use DSLR as a webcam

--- zsh/aliases/video.zsh
+++ zsh/aliases/video.zsh
@@ -1,1 +1,3 @@
 alias capture_desktop=$'ffmpeg -video_size $(xdpyinfo |awk \'/dimensions:/{printf $2}\') -framerate 60 -f x11grab -i :0.0 output.mp4'
+
+alias eos_webcam='gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0'
