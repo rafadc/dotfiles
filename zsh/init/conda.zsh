8702bdb36c206343f66da648b2c94ab0eb1adc1a
Rafa de Castro <rafael@micubiculo.com>
2021-04-07 00:18:08 +0200

    Added conda

---
+++ zsh/init/conda.zsh
@@ -0,0 +1,14 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/rafadc/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/rafadc/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/rafadc/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/rafadc/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
