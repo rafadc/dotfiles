a759655f167c39fdfcaf0828b058ff09a51f9ac8
Rafa de Castro <rafael@micubiculo.com>
2021-07-26 10:08:42 +0200

    Added docker alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,10 @@ starship init fish | source
 
 alias gui=gitui
 alias v=nvim
+
+function take
+  mkdir $argv
+  cd $argv
+end
+
+alias docker-restart='sudo systemctl restart docker && docker-stop-all'
