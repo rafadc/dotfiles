4753eb3b1c8042955cfec6178283cd4536f2a19a
Rafa de Castro <rafael@micubiculo.com>
2023-10-21 09:25:11 +0200

    Added back xmonad. Custom compilation

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -1,8 +1,38 @@
-import XMonad
+import Custom.MyPolybar (myPolybar)
 
-main = xmonad def
+import XMonad
+import XMonad.Util.SpawnOnce ( spawnOnce )
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.NamedScratchpad (scratchpadWorkspaceTag)
+import XMonad.Util.WorkspaceCompare (filterOutWs)
+import XMonad.Util.Hacks as Hacks
+
+defaults = def
     { terminal    = "kitty"
     , modMask     = mod4Mask
+    , startupHook        = myStartupHook   
     , borderWidth = 3
     }
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce "feh --bg-scale ~/Pictures/Wallpapers/download.jpeg"
+  spawnOnce "dunst"
+  spawnOnce "telegram-desktop"
+  spawnOnce "flameshot"
+  spawnOnce "~/bin/overGrive-3.5.2-x86_64.AppImageoverGrive-3.5.2-x86_64.AppImage"
+
+
+main :: IO ()
+main = xmonad 
+        $ Hacks.javaHack 
+        $ withSB withSB myPolybar 
+        $ addEwmhWorkspaceSort (pure (filterOutWs [scratchpadWorkspaceTag]))
+      {- force XMonad to *not* set _NET_DESKTOP_VIEWPORT available since commit cf13f8f (https://github.com/xmonad/xmonad-contrib/commit/cf13f8f9)
+       - correct polybar order on dual monitors -}
+      . disableEwmhManageDesktopViewport
+      . ewmh $ defaults
+
+
+
 
