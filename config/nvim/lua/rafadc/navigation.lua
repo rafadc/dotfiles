6a047613e678e4df19df263f57c2f102e56435a6
Rafa de Castro <rafael@micubiculo.com>
2023-11-13 10:25:40 +0100

    Added file grepping

--- config/nvim/lua/rafadc/navigation.lua
+++ config/nvim/lua/rafadc/navigation.lua
@@ -6,6 +6,7 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files)
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
