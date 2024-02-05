7c60ddeb140decee11f3e65c664565a5284d92db
Rafa de Castro <rafael@micubiculo.com>
2024-02-05 09:43:14 +0100

    Added virtual edit mode for block

--- config/nvim/lua/rafadc/set.lua
+++ config/nvim/lua/rafadc/set.lua
@@ -6,10 +6,13 @@ vim.opt.relativenumber = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+-- Tabs
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
@@ -26,6 +29,9 @@ vim.opt.clipboard = "unnamedplus"
 vim.o.completeopt="menu,menuone,noinsert,noselect"
 
 vim.opt.conceallevel = 1 -- Required for Obsidian
+
+-- Block mode edit
+vim.opt.virtualedit = "block"
 
 -- Cleaning trailing whitespace
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
