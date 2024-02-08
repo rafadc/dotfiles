e3e44f9b20daee4a6575ac6e594e21cee28d4cfb
Rafa de Castro <rafael@micubiculo.com>
2024-02-08 09:17:34 +0100

    Removing luasnip

--- config/nvim/lua/rafadc/lazyvim.lua
+++ config/nvim/lua/rafadc/lazyvim.lua
@@ -67,8 +67,6 @@ require("lazy").setup({
   {'hrsh7th/cmp-buffer'},
   {'hrsh7th/cmp-path'},
   {'hrsh7th/cmp-cmdline'},
-  {'L3MON4D3/LuaSnip'},
-  {'saadparwaiz1/cmp_luasnip'},
 
   { "folke/neodev.nvim", opts = {} },
 
