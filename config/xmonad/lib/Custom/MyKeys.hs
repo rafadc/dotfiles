ba3cf05a77446890382486001f19eebfdacb9f9b
Rafa de Castro <rafael@micubiculo.com>
2024-03-05 14:49:09 +0100

    Asking for confirm on exit

--- config/xmonad/lib/Custom/MyKeys.hs
+++ config/xmonad/lib/Custom/MyKeys.hs
@@ -1,12 +1,15 @@
 module Custom.MyKeys where
 
-import XMonad (spawn, X, sendMessage)
+import XMonad (spawn, X, sendMessage, def, io)
 import XMonad.Layout.ToggleLayouts (ToggleLayout(..), toggleLayouts)
+import XMonad.Prompt.ConfirmPrompt
+import System.Exit (exitSuccess)
 
 myKeys :: [(String, X ())]
 myKeys =
   [
     ("M-q", spawn "xmonad --recompile && xmonad --restart")
+  , ("M-S-q", confirmPrompt def "exit" $ io exitSuccess)
   , ("M-t", spawn "rofimoji")
   , ("M-f", sendMessage (Toggle "Full"))
   , ("M-n", spawn "kitty --name vifm -e vifm")
