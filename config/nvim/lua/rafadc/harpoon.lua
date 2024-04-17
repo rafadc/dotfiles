68b32a7c7dde1f7773c83333ab7fcb0dd1531ccc
Rafa de Castro <rafael@micubiculo.com>
2024-04-17 13:22:55 +0200

    Added next and prev harpoon shortcuts

--- config/nvim/lua/rafadc/harpoon.lua
+++ config/nvim/lua/rafadc/harpoon.lua
@@ -3,3 +3,6 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set('n', '<leader>jj', ui.toggle_quick_menu)
 vim.keymap.set('n', '<leader>ja', mark.add_file)
+
+vim.keymap.set('n', '<C-j>', ui.nav_next)
+vim.keymap.set('n', '<C-h>', ui.nav_prev)
