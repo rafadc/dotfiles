0fb97b758e0013e3d700b25b8d3ddbb1dfca6f49
Rafa de Castro <rafael@micubiculo.com>
2024-07-06 10:40:28 +0200

    Saving file before applying rubocop

--- config/nvim/lua/rafadc/lang/ruby.lua
+++ config/nvim/lua/rafadc/lang/ruby.lua
@@ -4,3 +4,10 @@ require('lspconfig')['ruby_lsp'].setup {
     capabilities = capabilities
 }
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, 'n', '<F4>', ':update<CR>:!bundle exec rubocop -A %<CR>', {noremap = true, silent = true})
+  end
+})
+
