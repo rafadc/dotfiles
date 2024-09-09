1f95d1daae7797942a0d4e86921180d8eae7d7ad
Rafa de Castro <rafael@micubiculo.com>
2024-09-10 01:55:31 +0200

    Moved some programs just for linux

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,22 +6,8 @@ ln -s ~/.dotfiles/bin/* ~/bin/
 
 echo "Setting up zsh..."
 if [[ "$OSTYPE" == "darwin"* ]]; then
-   brew tap d12frosted/emacs-plus
-   brew install git zsh boxes figlet tmux lsd fzf go-jira emacs-plus vim gnu-sed diff-so-fancy jq shellcheck highlight bat graphviz
+   brew install git zsh boxes figlet tmux lsd fzf vim gnu-sed diff-so-fancy jq shellcheck highlight bat graphviz
 fi
-
-echo "Installing krew"
-# See https://krew.sigs.k8s.io/docs/user-guide/setup/install/ to see if this is still relevant
-(
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
-)
-kubectl krew install konfig
 
 echo "Configuring pyenv"
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
@@ -35,25 +21,29 @@ ln -fs ~/.dotfiles/zsh/zshenv ~/.zshenv
 ln -fs ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -fs ~/.dotfiles/zsh/fzf.zsh ~/.fzf.zsh
 
-ln -fs ~/.dotfiles/xmodmap ~/.Xmodmap
+if [[ "$OSTYPE" == "linux"* ]]; then
+    ln -fs ~/.dotfiles/xmodmap ~/.Xmodmap
 
-echo "Symlinking binscripts"
-ln -s ~/.dotfiles/bin/arch-news.sh ~/bin/
-ln -s ~/.dotfiles/bin/xstream.sh ~/bin/
+    echo "Symlinking binscripts"
+    ln -s ~/.dotfiles/bin/arch-news.sh ~/bin/
+    ln -s ~/.dotfiles/bin/xstream.sh ~/bin/
 
-echo "Installing pulseaudio control script"
-curl https://raw.githubusercontent.com/marioortizmanero/polybar-pulseaudio-control/master/pulseaudio-control.bash --output ~/bin/pulseaudio-control
-chmod +x ~/bin/pulseaudio-control
+    echo "Installing pulseaudio control script"
+    curl https://raw.githubusercontent.com/marioortizmanero/polybar-pulseaudio-control/master/pulseaudio-control.bash --output ~/bin/pulseaudio-control
+    chmod +x ~/bin/pulseaudio-control
+fi
 
 echo "Symlinking ssh config"
 mkdir -p ~/.ssh
 ln -s ~/.dotfiles/ssh.config ~/.ssh/config
 
-echo "Setting up window management"
-ln -fs ~/.dotfiles/config/xmonad ~/.xmonad
-ln -fs ~/.dotfiles/config/xmonad/xmonad-custom.desktop ~/.local/share/applications/
-ln -fs ~/.dotfiles/config/dunst ~/.config/dunst
-ln -fs ~/.dotfiles/config/picom ~/.config/picom
+if [[ "$OSTYPE" == "linux"* ]]; then
+    echo "Setting up window management"
+    ln -fs ~/.dotfiles/config/xmonad ~/.xmonad
+    ln -fs ~/.dotfiles/config/xmonad/xmonad-custom.desktop ~/.local/share/applications/
+    ln -fs ~/.dotfiles/config/dunst ~/.config/dunst
+    ln -fs ~/.dotfiles/config/picom ~/.config/picom
+fi
 
 echo "Setting up programming config files"
 ln -fs ~/.dotfiles/tool-versions ~/.tool-versions
@@ -62,21 +52,26 @@ ln -fs ~/.dotfiles/jetbrains/ideavimrc ~/.ideavimrc
 
 echo "Setting up application specific configs"
 mkdir -p ~/.config
-ln -fs ~/.dotfiles/config/polybar ~/.config/polybar
-ln -fs ~/.dotfiles/config/rofi ~/.config/
-ln -fs ~/.dotfiles/config/rofimoji.rc ~/.config/rofimoji.rc
+if [[ "$OSTYPE" == "linux"* ]]; then
+    ln -fs ~/.dotfiles/config/polybar ~/.config/polybar
+    ln -fs ~/.dotfiles/config/rofi ~/.config/
+    ln -fs ~/.dotfiles/config/rofimoji.rc ~/.config/rofimoji.rc
+    ln -fs ~/.dotfiles/config/paru ~/.config/paru
+    ln -fs ~/.dotfiles/config/feh ~/.config/feh
+    ln -fs ~/.dotfiles/config/ulauncher ~/.config/ulauncher
+fi
+
 ln -fs ~/.dotfiles/config/starship.toml ~/.config/starship.toml
 ln -fs ~/.dotfiles/config/fish ~/.config/fish
 ln -fs ~/.dotfiles/config/kitty ~/.config/kitty
 ln -fs ~/.dotfiles/config/nvim ~/.config/nvim
-ln -fs ~/.dotfiles/config/paru ~/.config/paru
-ln -fs ~/.dotfiles/config/feh ~/.config/feh
-ln -fs ~/.dotfiles/config/ulauncher ~/.config/ulauncher
 
-echo "Creating user services. They are not enabled by default"
-ln -fs ~/.dotfiles/systemd/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
-systemctl --user enable ssh-agent
-systemctl --user start ssh-agent
+if [[ "$OSTYPE" == "linux"* ]]; then
+    echo "Creating user services. They are not enabled by default"
+    ln -fs ~/.dotfiles/systemd/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
+    systemctl --user enable ssh-agent
+    systemctl --user start ssh-agent
+fi
 
 echo "Setting up tmux..."
 ln -fs ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
