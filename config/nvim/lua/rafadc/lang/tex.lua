dc34ff43177cf90f770db5593b66ae90f61757f2
Rafa de Castro <rafael@micubiculo.com>
2024-11-26 16:42:35 +0100

    Configuring tex engine

--- config/nvim/lua/rafadc/lang/tex.lua
+++ config/nvim/lua/rafadc/lang/tex.lua
@@ -1,6 +1,10 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+vim.api.nvim_set_var('vimtex_compiler_engine', 'lualatex')
+vim.api.nvim_set_var('vimtex_quickfix_mode', 0)
+vim.api.nvim_set_var('tex_flavor', 'latex')
+
 keymap.set("n","<leader>lc", ":VimtexClean<CR>", {noremap = true})
 keymap.set("n","<leader>ll", ":VimtexCompile<CR>", opts)
 keymap.set("n","<leader>lv", ":VimtexView<CR>", opts)
