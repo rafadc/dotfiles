50191382f733b1b0a6c47da6da38f84ce2a99359
Rafa de Castro <rafael@micubiculo.com>
2025-03-31 15:21:37 +0200

    Scripts for change resolutions in mac

---
+++ bin/1080.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/homebrew/bin/displayplacer "id:D1A9EBAB-7DED-41BB-B7F9-77D38BED3786 res:1920x1080 hz:100 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0"
---
+++ bin/4k.sh
@@ -0,0 +1,1 @@
+ /opt/homebrew/bin/displayplacer "id:D1A9EBAB-7DED-41BB-B7F9-77D38BED3786 res:2560x1440 hz:100 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0"
